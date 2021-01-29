c02a2db131bf9f3163dd136324628ba6e6961e75
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-01-29 23:59:53 +0600

    add all functions references on top and use calloc with adding stnadrad library

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,14 +1,124 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
+//# Use Ten functions
+void clear(void);
+void collect_players_name(void);
+void players_input_organizer(int number, int player);
+void show_board(void);
+void show_winner_count(int is_end);
+void clear_and_show(int num);
+bool is_exist_number(int number);
+bool validate_input_range(int number);
+bool found_match(int player);
+bool is_winner(int player);
+
+//* Global Variables
 char player_one_name[15],
      player_two_name[15],
      tic_tac_to_board[3][3];
 
-int players_input[9],
+int *players_input = NULL,
     player_one_win_count = 0,
     player_two_win_count = 0,
     draw = 0;
+
+//* Main Body -------------------------------------------- Start
+void main(void) {
+
+    int player_one_input,
+        player_two_input,
+        count = 9,
+        count_draw = 0,
+        continue_or_end = 0;
+
+    while (count) {
+
+        if (count == 9) {
+            for (int i = 0; i < 3; i++)  for (int j = 0; j < 3; j++) {
+                tic_tac_to_board[i][j] = ' ';
+            }
+
+            players_input = calloc(9, 4);
+
+            if (!continue_or_end) collect_players_name();
+
+            clear_and_show(1);
+        }
+
+        //* Player One Turn__________________
+        while (count) {
+
+            printf("\n\t__%s's Turn__\nInput a number from 1-9: ", player_one_name);
+            scanf("%d", &player_one_input);
+
+            if (!validate_input_range(player_one_input)) continue;
+
+            if (is_exist_number(player_one_input)) continue;
+            
+            players_input_organizer(player_one_input, 1);
+            clear_and_show(1);
+
+            if (count < 6) if (is_winner(1)) {
+                count = 0; break;
+            } else count_draw++;
+
+            players_input[player_one_input - 1] = player_one_input;
+            count--; break;
+        }
+
+        //* Players Two Turn__________________
+        while (count) {
+
+            printf("\n\t__%s's Turn__\nInput a number from 1-9: ", player_two_name);
+            scanf("%d", &player_two_input);
+
+            if (!validate_input_range(player_two_input)) continue;
+
+            if (is_exist_number(player_two_input)) continue;
+
+            players_input_organizer(player_two_input, 2);
+            clear_and_show(1);
+
+            if (count < 6) if (is_winner(2)) {
+                count = 0; break;
+            } else count_draw++;
+
+            players_input[player_two_input - 1] = player_two_input;
+            count--; break;
+        }
+
+        //* End the game, When count = 0__________________
+        while (!count) {
+            
+            if (count_draw == 5) {
+                draw++;
+                clear_and_show(1);
+                printf("\n\t____MATCH DRAW____\n");
+            }
+            printf("\n\nEnter 1 for Continue or 0 for End: ");
+
+            scanf("%d", &continue_or_end);
+            printf("\n");
+
+            /** 
+             * if continue_or_end = 1 then the game will start again with existing players
+             * else the whole process will end and show the final winner__________________
+            */
+            if (continue_or_end == 1 || continue_or_end == 0) if (!continue_or_end) {
+                clear();
+                show_winner_count(0);
+                printf("\t___GAME OVER___\n");
+                break;
+            } else {
+                count = 9;
+                count_draw = 0;
+            }
+        }
+    }
+}
+
 
 //# For Clear Terminal
 void clear(void) {
@@ -178,99 +288,4 @@ bool is_winner(int player) {
     }
 
     return false;
-}
-
-//* Main Body -------------------------------------------- Start
-void main(void) {
-
-    int player_one_input,
-        player_two_input,
-        count = 9,
-        count_draw = 0,
-        continue_or_end = 0;
-
-    while (count) {
-
-        if (count == 9) {
-
-            for (int i = 0; i < 3; i++)  for (int j = 0; j < 3; j++) {
-                tic_tac_to_board[i][j] = ' ';
-            }
-            for (int i = 0; i < 9; i++) players_input[i] = 0;
-
-            if (!continue_or_end) collect_players_name();
-
-            clear_and_show(1);
-        }
-
-        //* Player One Turn__________________
-        while (count) {
-
-            printf("\n\t__%s's Turn__\nInput a number from 1-9: ", player_one_name);
-            scanf("%d", &player_one_input);
-
-            if (!validate_input_range(player_one_input)) continue;
-
-            if (is_exist_number(player_one_input)) continue;
-            
-            players_input_organizer(player_one_input, 1);
-            clear_and_show(1);
-
-            if (count < 6) if (is_winner(1)) {
-                count = 0; break;
-            } else count_draw++;
-
-            players_input[player_one_input - 1] = player_one_input;
-            count--; break;
-        }
-
-        //* Players Two Turn__________________
-        while (count) {
-
-            printf("\n\t__%s's Turn__\nInput a number from 1-9: ", player_two_name);
-            scanf("%d", &player_two_input);
-
-            if (!validate_input_range(player_two_input)) continue;
-
-            if (is_exist_number(player_two_input)) continue;
-
-            players_input_organizer(player_two_input, 2);
-            clear_and_show(1);
-
-            if (count < 6) if (is_winner(2)) {
-                count = 0; break;
-            } else count_draw++;
-
-            players_input[player_two_input - 1] = player_two_input;
-            count--; break;
-        }
-
-        //* End the game, When count = 0__________________
-        while (!count) {
-            
-            if (count_draw == 5) {
-                draw++;
-                clear_and_show(1);
-                printf("\n\t____MATCH DRAW____\n");
-            }
-            printf("\n\nEnter 1 for Continue or 0 for End: ");
-
-            scanf("%d", &continue_or_end);
-            printf("\n");
-
-            /** 
-             * if continue_or_end = 1 then the game will start again with existing players
-             * else the whole process will end and show the final winner__________________
-            */
-            if (continue_or_end == 1 || continue_or_end == 0) if (!continue_or_end) {
-                clear();
-                show_winner_count(0);
-                printf("\t___GAME OVER___\n");
-                break;
-            } else {
-                count = 9;
-                count_draw = 0;
-            }
-        }
-    }
 }
